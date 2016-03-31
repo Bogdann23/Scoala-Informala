75187e3a54c72e64e96b4dd77a3728066763c0cd
Bogdan Dumitrescu <bogdandumitrescu@hotmail.com>
2016-03-31 23:16:50 +0300

    Homework 27 februarie - 4 martie

---
+++ src/homeworkVariables/HomeworkStandardTypes.java
@@ -0,0 +1,48 @@
+package homeworkVariables;
+
+public class HomeworkStandardTypes {
+
+	public static void main(String[] args) {
+
+byte nr1 = 127;
+short nr2 = 32767;
+int nr3 = 1073741824;
+long nr4 = 922337203;
+float nr5 = 15.40f;
+double nr6 = 3.1452;
+boolean yes = true;
+char ch = 'a';
+
+System.out.println(nr1);
+System.out.println(nr2);
+System.out.println(nr3);
+System.out.println(nr4);
+System.out.println(nr5);
+System.out.println(nr6);
+System.out.println(yes);
+System.out.println(ch);
+
+//a few different values
+System.out.println();
+
+long nr41 = 1234_5678_9012_3456L;
+long nr42 = 999_99_9999L;
+
+System.out.println(nr41);
+System.out.println(nr42);
+
+float nr51 = 123.4f;
+
+System.out.println(nr51);
+
+double nr61 = 123e-2;
+double nr62 = 12_23.23;
+double nr63 = 1.234e2;
+
+System.out.println(nr61);
+System.out.println(nr62);
+System.out.println(nr63);
+
+	}
+
+}
